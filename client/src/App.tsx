c3d8a9ddcacfc58acc797f45d21f6725befc2627
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-07 15:24:10 +0100

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import AuthRoutes from "./Components/Routes/AuthRoutes";
 import Body from "./Components/Body/Body";
 import styled from "styled-components";
+import { allowAccess } from "./Components/Global/GlobalContext";
 
 function App() {
+  const context = React.useContext(allowAccess);
+
   return (
     <Container>
-      {/* <AuthRoutes /> */}
-      <Body />
+      <AuthRoutes />
+      {context?.userData.name === "" ? <Body /> : null}
     </Container>
   );
 }
