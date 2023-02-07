b2f8f4c3f302e1737c186853aefd9850021e3db0
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-07 11:49:24 +0100

    update

---
+++ client/src/Components/SideBar/SideBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../Header/Header";
+
+const SideBar = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default SideBar;
+
+const Container = styled.div``;
