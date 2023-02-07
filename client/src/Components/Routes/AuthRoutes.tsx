469643ab10860d3beb4e4a29b0553e8fc0c69c16
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-07 11:23:02 +0100

    udate

--- client/src/Components/Routes/AuthRoutes.tsx
+++ client/src/Components/Routes/AuthRoutes.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { useRoutes } from "react-router-dom";
 
 const AuthRoutes = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default AuthRoutes
+    const element = useRoutes([
+        {
+            path: "/",
+            // element: 
+        }
+    ])
+
+  return element;
+};
+
+export default AuthRoutes;
--- client/src/Components/Routes/Routes.tsx
+++ client/src/Components/Routes/Routes.tsx
@@ -1,15 +1,31 @@
-import React, {} from 'react'
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import AssignedToMe from "../Task/AssignedToMe";
+import Importance from "../Task/Importance";
+import Planned from "../Task/Planned";
+import Tasks from "../Task/Tasks";
 
 const Routes = () => {
-
-  const element = ([
+  const element = useRoutes([
     {
+      path: "/task",
+      element: <Tasks />,
+    },
+    {
+      path: "/planned",
+      element: <Planned />,
+    },
+    {
+      path: "/assigned",
+      element: <AssignedToMe />,
+    },
+    {
+      path: "/importance",
+      element: <Importance />,
+    },
+  ]);
 
-    }
-  ])
+  return element;
+};
 
-  return element
-  
-}
-
-export default Routes
+export default Routes;
